f617c29419eac7af1a434c8bd50e659ff71b84ad
Devgan <devansh.devgan@capgemini.com>
2025-03-12 18:07:57 +0530

    initialization

---
+++ src/components/Search.jsx
@@ -0,0 +1,10 @@
+export default function Search ({searchTerm, setSearchTerm}) {
+    return (
+        <div className="search">
+            <div>
+                <img src="search.svg" alt="search" />
+                <input type="text" placeholder="Search through thousand of movies" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+            </div>
+        </div>
+    )
+}
